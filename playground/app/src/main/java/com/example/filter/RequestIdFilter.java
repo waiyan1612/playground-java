cb056012a52469db22378eeae40e35300618e5f5
Wai Yan <waiyan1612@gmail.com>
2024-10-07 07:47:33 +0800

    Add RequestFilter to log request id with MDC

---
+++ playground/app/src/main/java/com/example/filter/RequestIdFilter.java
@@ -0,0 +1,39 @@
+package com.example.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.UUID;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestIdFilter extends OncePerRequestFilter {
+    private static final String REQUEST_ID = "mdc.request_id";
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws java.io.IOException, ServletException {
+        MDC.put(REQUEST_ID, UUID.randomUUID().toString());
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove(REQUEST_ID);
+        }
+    }
+
+    @Override
+    protected boolean isAsyncDispatch(final HttpServletRequest request) {
+        return false;
+    }
+
+    @Override
+    protected boolean shouldNotFilterErrorDispatch() {
+        return false;
+    }
+}
